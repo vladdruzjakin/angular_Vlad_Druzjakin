f96d1a3646f3b961207ac139c07d37ec03c1be91
Vlad Druzjakin <drz.vladd@gmail.com>
2019-07-02 18:01:17 +0300

    homework1

---
+++ HW1/js/hw1.js
@@ -0,0 +1,34 @@
+// 1)Определить переменные для всех примитивом джавасткрипта
+var num = 1,
+    str = 'string',
+    bool = true,
+    obj = {},
+    arrayNew = [1, 2, 3, 4, 5, 6],
+    array = [1, 2, 3, 4, 5, 6],
+    arrCortage = [1, true, 'str'];
+console.log(array);
+console.log(arrayNew);
+// 2)Написать функцию принимающую число и 
+// возвращающую его факториал по всем правилам тайпскрипта
+function factorial(num) {
+    var fact = num;
+    for (var i = fact - 1; i >= 1; i--) {
+        fact *= i;
+    };
+    return fact;
+}
+console.log(factorial(3));
+// 3)Написать функцию принимающую
+//  число и возврашающую массив с числами фибоначи до этого порядкового числа
+function fibonacci(num) {
+    var arrFibo = [0, 1];
+    if (num <= 2) {
+        return arrFibo.slice(0, num);
+    } else {
+        for (var i = 2; i < num; i++) {
+            arrFibo.push(arrFibo[arrFibo.length - 1] + arrFibo[arrFibo.length - 2]);
+        }
+        return arrFibo;
+    }
+}
+console.log(fibonacci(10));
